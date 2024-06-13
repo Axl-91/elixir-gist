1dcec1b279f027c78e2ba6c32e16a03765fdd9a0
Axl Aparicio <axelaparicior@gmail.com>
2024-06-13 13:47:20 -0300

    Added code form and button

--- lib/elixir_gist_web/live/create_gist_live.ex
+++ lib/elixir_gist_web/live/create_gist_live.ex
@@ -1,7 +1,15 @@
 defmodule ElixirGistWeb.CreateGistLive do
+  alias ElixirGist.Gists
+  alias ElixirGist.Gists.Gist
   use ElixirGistWeb, :live_view
 
   def mount(_params, _session, socket) do
+    socket =
+      assign(
+        socket,
+        form: to_form(Gists.change_gist(%Gist{}))
+      )
+
     {:ok, socket}
   end
 end
